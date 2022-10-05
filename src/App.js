d0991a06a454ee31605c2528355c99d7c9fb0392
enochsori <enoch.ha915@gmail.com>
2022-10-05 08:42:35 -0400

    Bussiness logic

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Datepicker from "./components/DatePicker";
+
+function App() {
+  return (
+    <>
+      <div>DATE PICKER</div>
+      <Datepicker />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavButton.js
@@ -0,0 +1,16 @@
+export default function NavButton({ children, onClick }) {
+  return (
+    <button
+      type='button'
+      onClick={onClick}
+      css={{
+        border: "1px solid #929598",
+        background: "transparent",
+        padding: "8px",
+        fontSize: "12px",
+      }}
+    >
+      {children}
+    </button>
+  );
+}
